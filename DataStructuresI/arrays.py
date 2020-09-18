49496b7149070d4aff1eafe25e6cdd73f7cbb2c7
Alex Magana <46731475+jamcoding@users.noreply.github.com>
2020-09-17 21:05:20 -0500

    changes for section 1 and 2

--- DataStructuresI/arrays.py
+++ DataStructuresI/arrays.py
@@ -1,9 +1,28 @@
 # Linear time iterate over all items
+arr = [12, 23, 56, 87, 14] # n = 5
+
+for num in arr: # O(n * 1) ==> O(n)
+    print(num) # O(1)
+for num in arr: # O(n * 1) ==> O(n)
+    print(num) # O(1)
+
+# One for loop:
+# O(n) + O(1) = O(n) -> linear time complexity
+
+# Two for loops:
+# O(n * 1) + O(n * 1) + O(1) -> O(2n) + O(1) => O(n) + O(1) => O(n)
 
 # constant time lookup
+print(arr[3]) # O(1)
 
 # quadratic time nested iteration
+for x in arr: # O(n)
+    for y in arr: # O(n) => O(n^2)
+        print(x, y) # O(1) => O(1 * n^2)
 
+# O(n^2) + O(n) + O(1 * n^2)
+# O(2n^2) + O(n) => O(n^2) + O(n^2)
+# O(n^2)
 
 # can we do better?
 
